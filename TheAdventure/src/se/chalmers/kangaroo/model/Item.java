d34b9615c0f28e990d55558835a988816ad0ceb2
Sean Pavlov <sean.pavlov@gmail.com>
2012-03-26 16:29:28 +0200

    Updated Item-class with javadoc

--- TheAdventure/src/se/chalmers/kangaroo/model/Item.java
+++ TheAdventure/src/se/chalmers/kangaroo/model/Item.java
@@ -1,9 +1,26 @@
 package se.chalmers.kangaroo.model;
 
+/**
+ * 
+ * Item-interface is used to represent what all sort of items you can pick up
+ * has in common. When Kangaroo is using an Item, it will get a special ability
+ * that will be to its advantage.
+ * 
+ * @author pavlov
+ * 
+ */
 public interface Item {
-	
+
+	/**
+	 * This method is used to perform an action immediately after Kangaroo is
+	 * picking up an item.
+	 */
 	public void onPickup();
-	
+
+	/**
+	 * When the item is no longer in use, this method will deactivate the item
+	 * so you do not have the special abilities left.
+	 */
 	public void onDrop();
 
 }
