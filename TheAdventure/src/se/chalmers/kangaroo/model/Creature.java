17fbd9d8b1ddb947b4cf86f2ebd8b4aa5bfb3ae0
Simon Almgren <simon.almgren@hotmail.com>
2012-03-26 16:36:39 +0200

    added the Creature-class with javadoc.

---
+++ TheAdventure/src/se/chalmers/kangaroo/model/Creature.java
@@ -0,0 +1,43 @@
+package se.chalmers.kangaroo.model;
+/**
+ * This abstract class represents all the different hostile creatures in the game.
+ * @author simonal
+ *
+ */
+public abstract class Creature implements Movable{
+	
+	private Position spawnPos;
+	private boolean killable;
+	private Position pos;
+	/**
+	 * A protected constructor which takes a spawnposition and the direction the creature will be facing.
+	 * @param spawnPos
+	 * @param direction
+	 */
+	protected Creature(Position spawnPos, Direction direction) {
+		//TODO
+	}
+	/**
+	 * Changes the direction the creature is facing and possible moving.
+	 * @param newDirection
+	 */
+	public void changeDirection(Direction newDirection) {
+		//TODO
+	}
+	/**
+	 * Returns true of the creature is killable and false if it isnt.
+	 * @return killable
+	 */
+	public boolean isKillable() {
+		//TODO
+	}
+	/**
+	 * Moves the creature according to the delta-y nad delta-x.
+	 * @param dy
+	 * @param dx
+	 */
+	public void move(int dy, int dx) {
+		//TODO
+	}
+
+}
