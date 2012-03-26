5e08563b0be54008448fe35d35a17362c6a76ffb
Henrik Alburg <henrik.alburg@gmail.com>
2012-03-26 17:20:50 +0200

    Sean tricked me into changing one comment

--- TheAdventure/src/se/chalmers/kangaroo/model/TileFactory.java
+++ TheAdventure/src/se/chalmers/kangaroo/model/TileFactory.java
@@ -2,24 +2,27 @@ package se.chalmers.kangaroo.model;
 
 /**
  * Creates tiles or interactiveTiles depending on ID.
+ * 
  * @author alburgh
- *
+ * 
  */
 public class TileFactory {
-	private static String INTERACTIVE_TILES = "ABCDEFGH"; //TODO: CHANGE
-	
-	public TileFactory(){
-		
+	private static String INTERACTIVE_TILES = "ABCDEFGH"; // TODO: CHANGE
+
+	public TileFactory() {
+
 	}
+
 	/**
 	 * Creates Tiles (or interactiveTiles) based on ID.
+	 * 
 	 * @param c, the id of the tile
 	 * @return the tile created
 	 */
-	public Tile createTile(char c){
-		if( INTERACTIVE_TILES.contains("" + c) ){
+	public Tile createTile(char c) {
+		if (INTERACTIVE_TILES.contains("" + c)) {
 			return null; // <-interactivetile TODO
-		}else{
+		} else {
 			return new Tile(c);
 		}
 	}
