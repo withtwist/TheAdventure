4802d98d6bf62a4ab87a3dcb50fc887c86d765e1
Shortleft <arvid.b.karlsson@gmail.com>
2012-03-26 16:50:27 +0200

    More comments in the class GameModel

--- TheAdventure/src/se/chalmers/kangaroo/model/GameModel.java
+++ TheAdventure/src/se/chalmers/kangaroo/model/GameModel.java
@@ -1,27 +1,42 @@
 package se.chalmers.kangaroo.model;
+
 /**
  * A class to represent the model of a platform game.
+ * 
  * @author arvidk
- *
+ * 
  */
 public class GameModel {
 
+	/*
+	 * The kangaroo that the player controlls.
+	 */
 	private Kangaroo kangaroo;
+	/*
+	 * The current level that the player are playing.
+	 */
 	private GameMap map;
+	/*
+	 * The amount of times the player have died.
+	 */
 	private int deathCount;
+	/*
+	 * Will keep track of how long the player have played.
+	 */
 	private int time;
-	
+
 	/**
 	 * A method to update the game.
 	 */
-	public void update(){
-		//TODO implement update
+	public void update() {
+		// TODO implement update
 	}
+
 	/**
 	 * 
 	 * @return the time that has elapsed for the player.
 	 */
-	public int getTime(){
+	public int getTime() {
 		return time;
 	}
 }
