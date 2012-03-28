f9047532c7b3529448da4e578f761351a83ee690
Henrik Alburg <henrik.alburg@gmail.com>
2012-03-28 10:07:03 +0200

    added the fileToMap method, will probably be moved to a layer under later

--- TheAdventure/src/se/chalmers/kangaroo/model/GameMap.java
+++ TheAdventure/src/se/chalmers/kangaroo/model/GameMap.java
@@ -1,7 +1,11 @@
 package se.chalmers.kangaroo.model;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * This is a class for representing a map. It consists of a matrix of gametiles.
@@ -49,5 +53,47 @@ public class GameMap {
 	 */
 	public List<Creature> getCreatures() {
 		return creatures;
+	}
+	
+	/**
+	 * A method for reading a tmx-file to a matrix of IDs. It is important that
+	 * the layer in the .tmx is called "tiles" and that it is not compressed or
+	 * encoded.
+	 * 
+	 * @param fileName
+	 *            , the filename to the map
+	 * @return null if unsuccessful, otherwise a matrix of Strings where each
+	 *         String is an id.
+	 */
+	private static String[][] readFileToMap(String fileName) {
+		String[][] tileId = null;
+		try {
+			InputStream in = new FileInputStream(fileName);
+			Scanner sc = new Scanner(in);
+
+			while (sc.hasNextLine()) {
+				String tmp = sc.nextLine();
+				if (tmp.contains("name=\"tiles\"")) {
+					String[] split = tmp.split("\"");
+					int width = Integer.parseInt(split[3]);
+					int height = Integer.parseInt(split[5]);
+					tileId = new String[width][height];
+					sc.nextLine(); // Jump over the <data> line
+					for (int i = 0; i < height; i++) {
+						for (int j = 0; j < width; j++) {
+							String tiles = sc.nextLine().substring(14);
+							tiles = tiles.substring(0, tiles.length() - 3);
+							tileId[j][i] = tiles;
+						}
+					}
+				}
+			}
+			sc.close();
+			in.close();
+
+		} catch (IOException e) {
+			System.out.println("Wrong filename!");
+		}
+		return tileId;
 	}
 }
