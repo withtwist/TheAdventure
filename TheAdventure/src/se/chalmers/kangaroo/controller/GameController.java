16208c8197646e6923a383391e41ac0bf57904db
Simon Almgren <simon.almgren@hotmail.com>
2012-04-17 16:06:03 +0200

    updated GameController

--- TheAdventure/src/se/chalmers/kangaroo/controller/GameController.java
+++ TheAdventure/src/se/chalmers/kangaroo/controller/GameController.java
@@ -1,5 +1,28 @@
 package se.chalmers.kangaroo.controller;
 
-public class GameController {
+import se.chalmers.kangaroo.model.GameModel;
+
+/**
+ * A class for handling and running the game.
+ * @author twist3r
+ *
+ */
+public class GameController{
+	
+	private GameModel game;
+	
+	public GameController() {
+		
+		game = new GameModel();
+		
+	}
+	/**
+	 * Updates the entire GameModel.
+	 */
+	public void update() {
+		
+		game.update();
+		
+	}
 
 }
