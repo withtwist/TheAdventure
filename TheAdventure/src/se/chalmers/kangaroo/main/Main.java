199408eb6ac1f2866ef084e58b658c3952b841fb
Henrik Alburg <henrik.alburg@gmail.com>
2012-04-18 11:09:35 +0200

    added GameView and changed main so GameView would run

--- TheAdventure/src/se/chalmers/kangaroo/main/Main.java
+++ TheAdventure/src/se/chalmers/kangaroo/main/Main.java
@@ -1,10 +1,10 @@
 package se.chalmers.kangaroo.main;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
+import se.chalmers.kangaroo.model.GameModel;
 import se.chalmers.kangaroo.utils.Sound;
+import se.chalmers.kangaroo.view.GameView;
 import se.chalmers.kangaroo.view.MenuView;
 
 public class Main {
@@ -19,8 +19,8 @@ public class Main {
 		jf.setSize(1024,576);
 		jf.setResizable(false);
 		MenuView gw = new MenuView("resources/images/background.gif");
-
-		jf.add(gw);
+		GameView gameview = new GameView("resources/images/background.gif", new GameModel());
+		jf.add(gameview);
 		gw.revalidate();
 		Sound s = new Sound("resources/music/menuemusic.wav");
 		s.play();
---
+++ TheAdventure/src/se/chalmers/kangaroo/view/GameView.java
@@ -0,0 +1,35 @@
+package se.chalmers.kangaroo.view;
+
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+
+import se.chalmers.kangaroo.model.GameModel;
+
+/**
+ * 
+ * @author alburgh
+ *
+ */
+public class GameView extends JPanelWithBackground{
+	private GameModel model;
+	
+	public GameView(String imagepath, GameModel gm) {
+		super(imagepath);
+		model = gm;
+	}
+	
+	public void update(){
+		
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		ImageIcon img = new ImageIcon("../gfx/tiles/tile_" +1+".png");
+		img.paintIcon(null, g, 0, 0);
+		img = new ImageIcon("../gfx/tiles/tile_" +1+".png");
+		img.paintIcon(null, g, 32, 32);
+	}
+
+}
