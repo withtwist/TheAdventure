eef06be4153fc46d3831fef5ba812038e29df84f
Shortleft <arvid.b.karlsson@gmail.com>
2012-04-18 11:11:30 +0200

    Added Constants

---
+++ TheAdventure/src/se/chalmers/kangaroo/constants/Constants.java
@@ -0,0 +1,16 @@
+package se.chalmers.kangaroo.constants;
+
+/**
+ * A class to keep track of all constants.
+ * A class with only public static final variables.
+ * @author Arvid
+ *
+ */
+public  class Constants {
+	/*
+	 * A constant to keep track of the tile size so
+	 * we can check collition.
+	 */
+	public static final int TILE_SIZE = 32;
+	
+}
