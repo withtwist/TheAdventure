82c688d170002cf9db6b7c3b7daff78945b4568d
Shortleft <arvid.b.karlsson@gmail.com>
2012-04-18 16:17:57 +0200

    Last commit of the day. Not sure if anything needs to be commited.

--- TheAdventure/src/se/chalmers/kangaroo/model/GameModel.java
+++ TheAdventure/src/se/chalmers/kangaroo/model/GameModel.java
@@ -15,7 +15,7 @@ public class GameModel {
 	 * The kangaroo that the player controlls.
 	 */
 	private Kangaroo kangaroo;
-	
+
 	private Creature creature;
 	/*
 	 * The current level that the player are playing.
@@ -33,40 +33,41 @@ public class GameModel {
 	 * Will check if the game is running.
 	 */
 	private boolean isRunning = false;
-	
+
 	private GameMap gameMap;
-	
+
 	private Tile tile;
-	
-	
+
+	private Position oldPos;
+
 	/**
 	 * A method to start the game.
 	 */
-//	public void start() {
-//		isRunning = true;
-//		while (isRunning) {
-//			this.update();
-//		}
-//	}
+	// public void start() {
+	// isRunning = true;
+	// while (isRunning) {
+	// this.update();
+	// }
+	// }
 
 	/**
 	 * Will make it able to pause the game
 	 */
-//	public void pause() {
-//		if (isRunning = true) {
-//			isRunning = false;
-//		} else {
-//			this.start();
-//		}
-//	}
+	// public void pause() {
+	// if (isRunning = true) {
+	// isRunning = false;
+	// } else {
+	// this.start();
+	// }
+	// }
 
 	/**
 	 * A method to stop the game, and thereby quit it.
 	 */
-//	public void stop() {
-//		isRunning = false;
-//	}
-	
+	// public void stop() {
+	// isRunning = false;
+	// }
+
 	public GameModel() {
 		gameMap = new GameMap("../maps/testmap.tmx");
 		kangaroo = new Kangaroo(new Position(10, 186));
@@ -76,36 +77,39 @@ public class GameModel {
 	 * A method to update the game.
 	 */
 	public void update() {
-		// TODO implement update
+		oldPos = kangaroo.getPosition();
+		checkCollition();
 	}
 
 	/**
 	 * Checks if a polygon collides with a tile
 	 */
 	private void checkCollition() {
-		if(kangaroo.getPolygon().getBounds2D().intersects(creature.getPolygon().getBounds2D())) {
-			if(kangaroo.getVerticalSpeed() > 0 && creature.isKillable()){
+
+		if (kangaroo.getPolygon().getBounds2D()
+				.intersects(creature.getPolygon().getBounds2D())) {
+			if (kangaroo.getVerticalSpeed() > 0 && creature.isKillable()) {
 				creature.remove();
 			} else {
 				deathCount++;
 				restartLevel();
 			}
 		}
-		if(tile.isCollidable() &&  kangaroo.getPolygon().getBounds2D().intersects(tile.getPolygon().getBounds2D())) {
-			
+
+		if (tile.isCollidable()
+				&& kangaroo.getPolygon().getBounds2D()
+						.intersects(tile.getPolygon().getBounds2D())) {
+			kangaroo.setPosition(oldPos);
 		}
 	}
-	
-	
-	
+
 	/**
-	 * Restarts the level.
-	 * Will be used when the kangaroo dies.
+	 * Restarts the level. Will be used when the kangaroo dies.
 	 */
-	private void restartLevel(){
-		//TODO implement restartLevel
+	private void restartLevel() {
+		// TODO implement restartLevel
 	}
-	
+
 	/**
 	 * 
 	 * @return the amount of times the player has died.
@@ -113,7 +117,7 @@ public class GameModel {
 	public int getDeathCount() {
 		return deathCount;
 	}
-	
+
 	/**
 	 * 
 	 * @return the kangaroo
@@ -121,7 +125,7 @@ public class GameModel {
 	public Kangaroo getKangaroo() {
 		return kangaroo;
 	}
-	
+
 	/**
 	 * 
 	 * @return the gameMap
