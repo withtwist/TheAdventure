4e724c08bb78cd0e7f5a53ecc5f27b79ea5ff5e0
Simon Almgren <simon.almgren@hotmail.com>
2012-04-18 10:30:12 +0200

    removed GameView

--- TheAdventure/src/se/chalmers/kangaroo/main/Main.java
+++ TheAdventure/src/se/chalmers/kangaroo/main/Main.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import se.chalmers.kangaroo.utils.Sound;
-import se.chalmers.kangaroo.view.GameView;
+import se.chalmers.kangaroo.view.MenuView;
 
 public class Main {
 
@@ -18,7 +18,7 @@ public class Main {
 		jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE);
 		jf.setSize(1024,576);
 		jf.setResizable(false);
-		GameView gw = new GameView("resources/images/background.gif");
+		MenuView gw = new MenuView("resources/images/background.gif");
 
 		jf.add(gw);
 		gw.revalidate();
