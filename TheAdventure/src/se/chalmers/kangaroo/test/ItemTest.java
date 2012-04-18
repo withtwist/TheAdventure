5d21af238a5cd5362b2d72ce0b277d50d982954f
Simon Almgren <simon.almgren@hotmail.com>
2012-04-18 10:36:28 +0200

    added a package for tests and added ItemTest

---
+++ TheAdventure/src/se/chalmers/kangaroo/test/ItemTest.java
@@ -0,0 +1,14 @@
+package se.chalmers.kangaroo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ItemTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
