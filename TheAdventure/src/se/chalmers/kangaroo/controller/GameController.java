47d7e4af747ef0abfbb5c302548243b58faaf100
Henrik Alburg <henrik.alburg@gmail.com>
2012-04-18 16:17:24 +0200

    added loop to controller

--- TheAdventure/src/se/chalmers/kangaroo/controller/GameController.java
+++ TheAdventure/src/se/chalmers/kangaroo/controller/GameController.java
@@ -1,6 +1,7 @@
 package se.chalmers.kangaroo.controller;
 
 import se.chalmers.kangaroo.model.GameModel;
+import se.chalmers.kangaroo.view.GameView;
 
 /**
  * A class for handling and running the game.
@@ -9,20 +10,32 @@ import se.chalmers.kangaroo.model.GameModel;
  */
 public class GameController{
 	
-	private GameModel game;
+	private GameModel gm;
+	private GameView gv; 
 	
-	public GameController() {
+	public GameController(GameModel model, GameView view) {
 		
-		game = new GameModel();
-		
-	}
-	/**
-	 * Updates the entire GameModel.
-	 */
-	public void update() {
-		
-		game.update();
-		
+		gm = model;
+		gv = view;
 	}
 
+	public void start(){
+		new PlayModel().run();
+	}
+	
+	class PlayModel implements Runnable {
+		public void run() {
+			while (true) {
+				try {
+					Thread.sleep(16);
+				} catch (InterruptedException ex) {
+					System.out
+							.println("Error, you woke the sleeping bear.. o_O");
+				}
+				gm.update();
+				gv.revalidate();
+				gv.repaint();
+			}
+		}
+	}
 }
