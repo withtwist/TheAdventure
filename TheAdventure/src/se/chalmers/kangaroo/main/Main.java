0c8c27c3c8f41c962a5c77f5482e1a68e61b556e
Simon Almgren <simon.almgren@hotmail.com>
2012-04-18 13:57:27 +0200

    updated testmapp and Main class

--- TheAdventure/src/se/chalmers/kangaroo/main/Main.java
+++ TheAdventure/src/se/chalmers/kangaroo/main/Main.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 import se.chalmers.kangaroo.model.GameModel;
 import se.chalmers.kangaroo.utils.Sound;
-import se.chalmers.kangaroo.view.GameView;
+import se.chalmers.kangaroo.view.MenuView;
 import se.chalmers.kangaroo.view.MenuView;
 
 public class Main {
@@ -19,7 +19,7 @@ public class Main {
 		jf.setSize(1024,576);
 		jf.setResizable(false);
 		MenuView gw = new MenuView("resources/images/background.gif");
-		GameView gameview = new GameView("resources/images/background.gif", new GameModel());
+		MenuView gameview = new MenuView("resources/images/background.gif", new GameModel());
 		jf.add(gameview);
 		gw.revalidate();
 		Sound s = new Sound("resources/music/menuemusic.wav");
