19ac8f18561655caa091cf5a2ad3c939e3d30784
Henrik Alburg <henrik.alburg@gmail.com>
2012-04-19 16:28:28 +0200

    added a method for drawing the view based on where the kangaroo is

--- TheAdventure/src/se/chalmers/kangaroo/view/GameView.java
+++ TheAdventure/src/se/chalmers/kangaroo/view/GameView.java
@@ -1,20 +1,19 @@
 package se.chalmers.kangaroo.view;
 
-
-
 import javax.swing.ImageIcon;
 
+import se.chalmers.kangaroo.constants.Constants;
 import se.chalmers.kangaroo.model.GameModel;
 import se.chalmers.kangaroo.model.Position;
 
 /**
  * 
  * @author alburgh
- *
+ * 
  */
-public class GameView extends JPanelWithBackground{
+public class GameView extends JPanelWithBackground {
 	private GameModel gm;
-	
+
 	public GameView(String imagepath, GameModel gm) {
 		super(imagepath);
 		this.gm = gm;
@@ -23,16 +22,26 @@ public class GameView extends JPanelWithBackground{
 	@Override
 	public void paintComponent(java.awt.Graphics g) {
 		super.paintComponent(g);
-		for(int y = 0; y < gm.getGameMap().getTileHeight(); y++)
-			for(int x = 0; x < gm.getGameMap().getTileWidth(); x++){
-				ImageIcon i = new ImageIcon("../gfx/tiles/tile_"+
-						gm.getGameMap().getTile(x, y).getId()+".png");
-				i.paintIcon(null, g, x*32, y*32);
-				
+		int drawFrom = getLeftX();
+		for (int y = 0; y < gm.getGameMap().getTileHeight(); y++)
+			for (int x = drawFrom; x < drawFrom + 32; x++) {
+				ImageIcon i = new ImageIcon("../gfx/tiles/tile_"
+						+ gm.getGameMap().getTile(x, y).getId() + ".png");
+				i.paintIcon(null, g, x * 32, y * 32);
+
 			}
 		Position p = gm.getKangaroo().getPosition();
-		new ImageIcon("../gfx/kangaroo/kangaroo_58x64_right.png").paintIcon(null, g, p.getX(), p.getY());
+		new ImageIcon("../gfx/kangaroo/kangaroo_58x64_right.png").paintIcon(
+				null, g, p.getX(), p.getY());
 	}
-	
-	
+
+	private int getLeftX() {
+		int kPos = gm.getKangaroo().getPosition().getX() / Constants.TILE_SIZE;
+		if (kPos < 32)
+			return 0;
+		if (gm.getGameMap().getTileWidth() - kPos < 32)
+			return gm.getGameMap().getTileWidth() - 32;
+
+		return kPos - 16;
+	}
 }
