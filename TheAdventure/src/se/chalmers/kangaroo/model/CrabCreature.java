6b868e193ea218fbece8a3f2f8105c60869feb69
Sean Pavlov <sean.pavlov@gmail.com>
2012-04-19 13:53:46 +0200

    Updated CrabCreature with a functional polygon

--- TheAdventure/src/se/chalmers/kangaroo/model/CrabCreature.java
+++ TheAdventure/src/se/chalmers/kangaroo/model/CrabCreature.java
@@ -17,11 +17,10 @@ public class CrabCreature extends Creature{
 		creaturePoly.npoints = 12;
 		int polyX[] = {0,14,14,20,20,44,44,50,50,64,64,54,54,10,10,0};
 		int polyY[] = {2,2,0,0,6,6,0,0,2,2,16,16,32,32,16,16};
-		if(polyX.length == polyY.length){
-			for(int i = 0; i < polyX.length; i++){
-				
-			}
+		for(int i = 0; i < polyX.length; i++){
+			creaturePoly.addPoint(polyX[i], polyY[i]);
 		}
+	
 	}
 
 }
