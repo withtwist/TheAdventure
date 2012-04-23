115f7a21bf4046f7ac033474e59711cf6f103a7e
Sean Pavlov <sean.pavlov@gmail.com>
2012-04-23 09:03:18 +0200

    Changed name to RedBlueButton and GameTimer and added an item StopTimeItem

---
+++ TheAdventure/src/se/chalmers/kangaroo/model/StopTimeItem.java
@@ -0,0 +1,23 @@
+package se.chalmers.kangaroo.model;
+
+public class StopTimeItem implements Item{
+
+	@Override
+	public void onPickup(Kangaroo k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDrop(Kangaroo k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUse(Kangaroo k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
