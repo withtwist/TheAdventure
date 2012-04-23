c78d2f84a3f5894ea1ac433a4d3bc9efc5146fe5
Henrik Alburg <henrik.alburg@gmail.com>
2012-04-23 10:43:33 +0200

    added comments to the previous class (and a bugfix) also added the file customkeys.txt where the custom keys will be saved

--- TheAdventure/src/se/chalmers/kangaroo/io/CustomKeysIO.java
+++ TheAdventure/src/se/chalmers/kangaroo/io/CustomKeysIO.java
@@ -13,13 +13,58 @@ import java.util.Scanner;
 
 import se.chalmers.kangaroo.controller.CustomKeys;
 
+/**
+ * This is a class that will save your custom keys to a file. You will of course
+ * be able to load them as well.
+ * 
+ * @author Henrik
+ * 
+ */
 public class CustomKeysIO {
 	private static CustomKeysIO instance;
 	private static CustomKeys ck;
 	private static int[] keys = new int[4];
 	private static final String FILE_NAME = "/resources/customkeys.txt";
 
+	/* Private constructor, so only one instance will be created. */
 	private CustomKeysIO() {
+		loadKeys();
+	}
+
+	/**
+	 * Returns the only instance of this class.
+	 * @return
+	 */
+	public static CustomKeysIO getInstance() {
+		return instance;
+	}
+	/**
+	 * Returns the current keys set in the file.
+	 * @return the key codes. 
+	 */
+	public static int[] getKeys() {
+		loadKeys();
+		return keys;
+	}
+	/**
+	 * Use the KeyEvent.keycodes to set your keys.
+	 * You can then use getKEys to return them.
+	 * @param key, the array of key codes. 
+	 */
+	public static void setKeys(int[] key) {
+		try {
+			Writer w = new FileWriter(FILE_NAME);
+			for (int i = 0; i < key.length + 3; i += 2) {
+				w.write(key[i] + "\\n");
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("There is no such file o_O");
+		} catch (IOException e) {
+			System.out.println("Something went wrong with the io");
+		}
+	}
+	/* Used to make sure that the latest version of the keys are returned. */
+	private static void loadKeys() {
 		try {
 			InputStream in = new FileInputStream(FILE_NAME);
 			Scanner sc = new Scanner(in);
@@ -31,27 +76,6 @@ public class CustomKeysIO {
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("No such file exists xD");
-		}
-	}
-
-	public static CustomKeysIO getInstance() {
-		return instance;
-	}
-
-	public static int[] getKeys() {
-		return keys;
-	}
-
-	public static void setKeys(int[] key) {
-		try {
-			Writer w = new FileWriter(FILE_NAME);
-			for (int i = 0; i < key.length + 3; i += 2) {
-				w.write(key[i]+"\\n");
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("There is no such file o_O");
-		} catch (IOException e) {
-			System.out.println("Something went wrong with the io");
 		}
 	}
 
