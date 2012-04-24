ff34afeac43b984452728b22cc56ceb4bae27764
Henrik Alburg <henrik.alburg@gmail.com>
2012-04-24 13:52:12 +0200

    added some changes that moves the camera along with the kangaroo, a bit laggy, but it works ok

--- TheAdventure/src/se/chalmers/kangaroo/view/GameView.java
+++ TheAdventure/src/se/chalmers/kangaroo/view/GameView.java
@@ -1,5 +1,7 @@
 package se.chalmers.kangaroo.view;
 
+import java.awt.Polygon;
+
 import javax.swing.ImageIcon;
 
 import org.newdawn.slick.Graphics;
@@ -15,14 +17,14 @@ import se.chalmers.kangaroo.model.Position;
  */
 public class GameView extends JPanelWithBackground {
 	private GameModel gm;
-	
-	//private Graphics slickGraphics = new Graphics();
+
+	// private Graphics slickGraphics = new Graphics();
 
 	public GameView(String imagepath, GameModel gm) {
 		super(imagepath);
 		this.gm = gm;
 	}
-	
+
 	@Override
 	public void paintComponent(java.awt.Graphics g) {
 		super.paintComponent(g);
@@ -33,23 +35,30 @@ public class GameView extends JPanelWithBackground {
 			for (int x = drawFrom; x < drawFrom + 32; x++) {
 				ImageIcon i = new ImageIcon("../gfx/tiles/tile_"
 						+ gm.getGameMap().getTile(x, y).getId() + ".png");
-				i.paintIcon(null, g, x * 32 , y * 32);
+				i.paintIcon(null, g, (x - drawFrom) * 32, y * 32);
 
 			}
-		//slickGraphics.drawAnimation(gm.getKangaroo().getAnimation(), p.getX()-32, p.getY()-32);
-
-		new ImageIcon("../gfx/kangaroo/kangaroo_58x64_right.png").paintIcon(
-				null, g, p.getX()-32, p.getY()-32);
-		g.drawPolygon(gm.getKangaroo().getPolygon());
+		// slickGraphics.drawAnimation(gm.getKangaroo().getAnimation(),
+		// p.getX()-32, p.getY()-32);
+		if (drawFrom == 0 || drawFrom == gm.getGameMap().getTileWidth() - 32) {
+			new ImageIcon("../gfx/kangaroo/kangaroo_58x64_right.png")
+					.paintIcon(null, g, p.getX(), p.getY());
+			g.drawPolygon(gm.getKangaroo().getPolygon());
+		} else {
+			new ImageIcon("../gfx/kangaroo/kangaroo_58x64_right.png")
+					.paintIcon(null, g, 15*32, p.getY());
+			int[] xs = {15*32, 17*32, 17*32, 15*32};
+			int[] ys = {p.getY(), p.getY(), p.getY()+63, p.getY()+63};
+			g.drawPolygon(new Polygon(xs, ys, 4));
+		}
 	}
 
 	private int getLeftX() {
 		int kPos = gm.getKangaroo().getPosition().getX() / Constants.TILE_SIZE;
-		if (kPos < 32)
+		if (kPos < 16)
 			return 0;
-		if (gm.getGameMap().getTileWidth() - kPos < 32)
+		if (gm.getGameMap().getTileWidth() - kPos < 16)
 			return gm.getGameMap().getTileWidth() - 32;
-
 		return kPos - 16;
 	}
 }
