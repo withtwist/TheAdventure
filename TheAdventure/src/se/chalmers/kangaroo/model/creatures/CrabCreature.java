4eebaeb79f3d0e1e109b1365df25746ecfca9274
Sean Pavlov <sean.pavlov@gmail.com>
2012-04-25 10:24:57 +0200

    Updated the CrabCreture so it matches the updated Creature

--- TheAdventure/src/se/chalmers/kangaroo/model/creatures/CrabCreature.java
+++ TheAdventure/src/se/chalmers/kangaroo/model/creatures/CrabCreature.java
@@ -16,8 +16,8 @@ public class CrabCreature extends Creature{
 	private Polygon creaturePoly;
 	
 	
-	protected CrabCreature(Position spawnPos, Direction direction) {
-		super(spawnPos, direction);
+	protected CrabCreature(Direction direction) {
+		super(direction);
 		creaturePoly.npoints = 12;
 		int polyX[] = {0,14,14,20,20,44,44,50,50,64,64,54,54,10,10,0};
 		int polyY[] = {2,2,0,0,6,6,0,0,2,2,16,16,32,32,16,16};
@@ -27,4 +27,15 @@ public class CrabCreature extends Creature{
 	
 	}
 
+
+	@Override
+	public boolean isKillable() {
+		return true;
+	}
+
+
+	@Override
+	public Polygon getPolygon() {
+		return creaturePoly;
+	}
 }
