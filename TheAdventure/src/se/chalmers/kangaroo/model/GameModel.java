b5a6474a8479a812c6628996e544edc23dff594b
Simon Almgren <simon.almgren@hotmail.com>
2012-05-02 22:46:02 +0200

    updated updateCreature so the creatures change direction if the encounter an edge

--- TheAdventure/src/se/chalmers/kangaroo/model/GameModel.java
+++ TheAdventure/src/se/chalmers/kangaroo/model/GameModel.java
@@ -92,8 +92,15 @@ public class GameModel {
 
 	private void updateCreatures() {
 		for( int i = 0; i < gameMap.getCreatureSize(); i++){
-			gameMap.getCreatureAt(i).updateCreature();
+			Creature c = gameMap.getCreatureAt(i);
+			c.updateCreature();
+			Rectangle2D cRect = c.getPolygon().getBounds2D();
+			if(!gameMap.getTile((int)(cRect.getMinX()/32), (int)(cRect.getMaxY()/32)+1).isCollidable() || !gameMap.getTile((int)(cRect.getMinX()/32), (int)(cRect.getMaxY()/32)+1).isCollidable()) {
+				c.changeDirection();
+			}
+			
 		}
+		
 	}
 
 	/**
