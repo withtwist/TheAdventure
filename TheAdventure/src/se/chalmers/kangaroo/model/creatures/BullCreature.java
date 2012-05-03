2ad494b2f45670798eba151d7eb2f4c00743238f
Henrik Alburg <grubla@kanin1.(none)>
2012-05-03 09:53:59 +0200

    fixed another bug found by findBugs

--- TheAdventure/src/se/chalmers/kangaroo/model/creatures/BullCreature.java
+++ TheAdventure/src/se/chalmers/kangaroo/model/creatures/BullCreature.java
@@ -71,8 +71,6 @@ public class BullCreature implements Creature {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result
-				+ ((bullPolygon == null) ? 0 : bullPolygon.hashCode());
 		result = prime * result + health;
 		return result;
 	}
