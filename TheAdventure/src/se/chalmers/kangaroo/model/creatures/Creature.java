c6dee5081b44d649b2ee6603232d6393dca616b0
Henrik Alburg <henrik.alburg@gmail.com>
2012-05-06 23:55:05 +0200

    forgot to add the files moved

---
+++ TheAdventure/src/se/chalmers/kangaroo/model/creatures/Creature.java
@@ -0,0 +1,51 @@
+package se.chalmers.kangaroo.model.creatures;
+
+import java.awt.Polygon;
+
+import se.chalmers.kangaroo.model.Movable;
+import se.chalmers.kangaroo.model.utils.Position;
+
+/**
+ * This abstract class represents all the different hostile creatures in the
+ * game.
+ * 
+ * @author simonal
+ * @modified by arvidk
+ * 
+ */
+public interface Creature extends Movable {
+	/**
+	 * Returns true of the creature is killable and false if it isn't.
+	 * 
+	 * @return killable
+	 */
+	public boolean isKillable();
+
+	/**
+	 * Returns the specified creatures polygon.
+	 * 
+	 * @return
+	 */
+	public Polygon getPolygon();
+
+	/**
+	 * Moves the creature according to what direction it is moving in and its
+	 * speed.
+	 * 
+	 * @param dy
+	 * @param dx
+	 */
+	public void move();
+
+	/**
+	 * Updates the Creature.
+	 */
+	public void updateCreature();
+
+	public int getId();
+
+	public Position getPosition();
+
+	public void changeDirection();
+
+}
