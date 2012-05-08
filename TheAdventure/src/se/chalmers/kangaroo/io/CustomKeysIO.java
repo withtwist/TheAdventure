771a872566258469b7246ba0a9fc4505936a13a9
Henrik Alburg <henrik.alburg@gmail.com>
2012-05-08 12:35:22 +0200

    added methods for getting and setting the volumes(io).

--- TheAdventure/src/se/chalmers/kangaroo/io/CustomKeysIO.java
+++ TheAdventure/src/se/chalmers/kangaroo/io/CustomKeysIO.java
@@ -19,7 +19,8 @@ import java.util.Scanner;
 public class CustomKeysIO {
 	private static CustomKeysIO instance;
 	private static int[] customKeys = new int[4];
-	private static final String FILE_NAME = "resources/customkeys.txt";
+	private static final String KEYS_FILE_NAME = "resources/customkeys.txt";
+	private static final String SOUND_FILE_NAME = "resources/soundoptions.txt";
 
 	/* Private constructor, so only one instance will be created. */
 	private CustomKeysIO() {
@@ -55,7 +56,7 @@ public class CustomKeysIO {
 	 */
 	public void setKeys(int[] key) {
 		try {
-			Writer w = new FileWriter(FILE_NAME);
+			Writer w = new FileWriter(KEYS_FILE_NAME);
 			for (int i = 0; i < key.length; i++) {
 				w.write(key[i] + "\n");
 			}
@@ -70,7 +71,7 @@ public class CustomKeysIO {
 	/* Used to make sure that the latest version of the keys are returned. */
 	private static void loadKeys() {
 		try {
-			InputStream in = new FileInputStream(FILE_NAME);
+			InputStream in = new FileInputStream(KEYS_FILE_NAME);
 			Scanner sc = new Scanner(in);
 			int i = 0;
 			while (sc.hasNext()) {
@@ -81,9 +82,74 @@ public class CustomKeysIO {
 			in.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("No such file exists");
-		} catch(IOException i){
+		} catch (IOException i) {
 			System.out.println("Problem closing the file");
 		}
+	}
+
+	/**
+	 * A method for setting the backgorund and soundeffets volumes.
+	 * 
+	 * @param bg
+	 *            , the background volume
+	 * @param sfx
+	 *            , the sound effects volume
+	 */
+	public void saveVolume(double bg, double sfx) {
+		try {
+			Writer w = new FileWriter(SOUND_FILE_NAME);
+			w.write(bg + "\n");
+			w.write(sfx + "\n");
+		} catch (FileNotFoundException e) {
+			System.out.println("There is no such file");
+		} catch (IOException e) {
+			System.out.println("Something went wrong with the io writing");
+		}
+	}
+
+	/**
+	 * A method that will return the background volume Will return -1 if the
+	 * value cannot be read.
+	 * 
+	 * @return a value between 0 and 1
+	 */
+	public double getBgVolume() {
+		double bg = -1;
+		try {
+			InputStream in = new FileInputStream(KEYS_FILE_NAME);
+			Scanner sc = new Scanner(in);
+			bg = Double.parseDouble(sc.next());
+			sc.close();
+			in.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("No such file exists");
+		} catch (IOException i) {
+			System.out.println("Problem closing the file");
+		}
+		return bg;
+	}
+
+	/**
+	 * A method that will return the sound effects volume Will return -1 if the
+	 * value cannot be read.
+	 * 
+	 * @return a value between 0 and 1
+	 */
+	public double getSfxVolume() {
+		double sfx = -1;
+		try {
+			InputStream in = new FileInputStream(KEYS_FILE_NAME);
+			Scanner sc = new Scanner(in);
+			sc.next();
+			sfx = Double.parseDouble(sc.next());
+			sc.close();
+			in.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("No such file exists");
+		} catch (IOException i) {
+			System.out.println("Problem closing the file");
+		}
+		return sfx;
 	}
 
 }
