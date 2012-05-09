663c3319d101b34ccb755d0ad7e3e6b6b6b7f804
Simon Almgren <simon.almgren@hotmail.com>
2012-05-09 19:11:51 +0200

    commented IncSpeedItem

--- TheAdventure/src/se/chalmers/kangaroo/model/kangaroo/IncSpeedItem.java
+++ TheAdventure/src/se/chalmers/kangaroo/model/kangaroo/IncSpeedItem.java
@@ -2,6 +2,11 @@ package se.chalmers.kangaroo.model.kangaroo;
 
 import se.chalmers.kangaroo.model.utils.Position;
 
+/**
+ * A class representing an item that increases the speed of the kangaroo"
+ * @author simonal
+ *
+ */
 public class IncSpeedItem implements Item, Runnable {
 
 	private Position pos;
@@ -11,7 +16,12 @@ public class IncSpeedItem implements Item, Runnable {
 	private Kangaroo k;
 
 	private int id;
-
+	/**
+	 * The default constructor, taking the id nad the x and y cordinate of where the item is placed.
+	 * @param id
+	 * @param x
+	 * @param y
+	 */
 	public IncSpeedItem(int id, int x, int y) {
 		this.id = id;
 		this.pos = new Position(x, y);
