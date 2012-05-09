36935c58c267a46dbeeb0148f23cf9c251918227
Henrik Alburg <grubla@kanin1.(none)>
2012-05-09 10:51:25 +0200

    added a test for the gamemodel (not finished)

---
+++ TheAdventure/test/se/chalmers/kangaroo/model/GameModelTest.java
@@ -0,0 +1,23 @@
+package se.chalmers.kangaroo.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameModelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void deathCountTest(){
+		GameModel gm = new GameModel();
+		gm.start();
+		for(int i = 1; i <= 10; i++)
+			gm.restartLevel();
+		assertTrue(gm.getDeathCount() == 10);
+	}
+
+}
