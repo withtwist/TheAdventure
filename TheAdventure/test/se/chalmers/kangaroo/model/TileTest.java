7ee1eeebdd38b45b67b3f6ac98b354a06e2052e1
Simon Almgren <simon.almgren@hotmail.com>
2012-05-10 04:18:54 +0200

    added a test to Tile

---
+++ TheAdventure/test/se/chalmers/kangaroo/model/TileTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import se.chalmers.kangaroo.model.Tile;
+
+
+public class TileTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testIsCollidable() {
+		Tile notCollidable = new Tile(42, 1, 2);
+		Tile collidable = new Tile(1,4,2);
+		assertTrue(!notCollidable.isCollidable() && collidable.isCollidable());
+	}
+	
+	@Test
+	public void testGetId() {
+		Tile hej = new Tile(42,1,1);
+		assertTrue(hej.getId()==42);
+	}
+	
+	@Test
+	public void testSetId() {
+		Tile bla = new Tile(1,1,1);
+		bla.setId(42);
+		assertTrue(bla.getId() == 42);
+	}
+	
+	@Test
+	public void testGetPolygon() {
+		Tile hej = new Tile(42,13,37);
+		assertTrue(hej.getPolygon() instanceof polygon);
+	}
+	
+	@Test
+	public void testChangeId() {
+		Tile hej = new Tile(42,13,37);
+		hej.changeId(1337);
+		assertTrue(hej.getId()==1337);
+	}
+	
+	
+	
+}
