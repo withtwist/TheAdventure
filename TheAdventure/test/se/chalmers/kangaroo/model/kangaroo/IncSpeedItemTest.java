94284589a980481a307983a428b301d8b9ac648c
Shortleft <arvid.b.karlsson@gmail.com>
2012-05-21 12:20:58 +0200

    Started testing incspeeditem

--- TheAdventure/test/se/chalmers/kangaroo/model/kangaroo/IncSpeedItemTest.java
+++ TheAdventure/test/se/chalmers/kangaroo/model/kangaroo/IncSpeedItemTest.java
@@ -9,11 +9,17 @@ import se.chalmers.kangaroo.model.utils.Position;
 public class IncSpeedItemTest {
 
 	@Test
-	public void testOnPickup() {
-		IncSpeedItem isi = new IncSpeedItem(35, 2, 2);
+	public void testOnUse() {
+		IncSpeedItem isi = new IncSpeedItem(37, 2, 2);
 		Kangaroo k = new Kangaroo(new Position(2, 2));
-		isi.onPickup(k);
-		assertTrue(k.getItem() == isi);
-	}
+		isi.onUse(k);
+		float f = 0.0f;
+		for (int i = 0; i < 3000; i++) {
+			f = f + 0.1f;
+			k.setVerticalSpeed(f);
+		}
+		System.out.println(k.getVerticalSpeed());
+		assertTrue(k.getVerticalSpeed() > 14.0f);
 
+	}
 }
